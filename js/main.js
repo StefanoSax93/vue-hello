2860f529f59fa62d4c0443c8654c494607231ffd
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-09 14:05:42 +0200

    aggiunta esercizio

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+const appVue = new Vue({
+    //seleziono l'elemento html dove avviare vue
+    el: '#main',
+
+    data: {
+        message: 'Hello Vue!',
+        messageStyle: 'text-success display-1 font-weight-bold',
+    }
+});
